bcda4cca2ff57cb2856034325f97b4d92ac4eacb
Marc <mkirigaya8@gmail.com>
2025-08-22 15:08:09 -0500

    Update To_do_list_app.py

--- To_do_list_app.py
+++ To_do_list_app.py
@@ -2,16 +2,22 @@
 # First python application
 
 tasks = []
-
+#creating a function here to use for the view option.
+def view_tasks(tasks_list):
+    for task in tasks_list:
+        print(f"Tasks: {task}")
+#creating a function here to use for the add option.      
+def add_task(tasks_list):
+    new_task = input("What task do you want to add?: ")
+    tasks_list.append(new_task)
+    
 while True:
     choice = input("What would you like to do? (add, view, quit): ")
     if choice == "quit":
         break
     elif choice == "view":
-        for task in tasks:
-            print(f"Tasks: {task}")
+        view_tasks(tasks)
     elif choice == "add":
-        new_task = input("What task do you want to add?: ")
-        tasks.append(new_task)
+        add_task(tasks)
     else:
         print("Not a valid selection! Try Again")
